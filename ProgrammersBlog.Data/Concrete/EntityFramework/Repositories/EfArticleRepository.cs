90b24c13a69c059d18fcc535053847590ecab0d1
bsrkynk <busra.kaynak@ogr.sakarya.edu.tr>
2021-08-30 16:51:58 +0300

    Repository classes and dbcontext objects are complete.

---
+++ ProgrammersBlog.Data/Concrete/EntityFramework/Repositories/EfArticleRepository.cs
@@ -0,0 +1,25 @@
+﻿using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProgrammersBlog.Data.Concrete.EntityFramework.Repositories
+{
+    public class EfArticleRepository: EfEntityRepositoryBase<Article>,IArticleRepository
+    {
+        public EfArticleRepository(DbContext context) : base(context)
+        {
+
+        }
+
+        public IList<Article> GetArticlesByCategory(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
